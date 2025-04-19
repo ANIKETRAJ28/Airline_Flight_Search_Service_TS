065385cd863df2a72a5d64308f34b44ff3def88f
Aniket <aniketraj28042003@gmail.com>
2025-04-19 15:00:54 +0530

    add: config: added configs for db setup

---
+++ src/config/db.config.ts
@@ -0,0 +1,25 @@
+import pgtools from "pgtools";
+
+import { DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, DB_NAME } from "./env.config";
+
+const config = {
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+};
+
+const db_name = DB_NAME as string;
+
+pgtools
+  .createdb(config, db_name)
+  .then(() => {
+    console.log(`Database ${db_name} created successfully`);
+  })
+  .catch((error) => {
+    if (error.name === "duplicate_database") {
+      console.log(`Database ${db_name} already exists`);
+    } else {
+      console.error(`Error creating database ${db_name}:`, error);
+    }
+  });
