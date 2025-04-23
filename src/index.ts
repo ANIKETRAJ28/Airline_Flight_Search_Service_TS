c17cfa7113a6de40bebf5cd98295729d9befa02b
Aniket <aniketraj28042003@gmail.com>
2025-04-23 20:39:22 +0530

    add: index.js: added v1 route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 import { PORT } from './config/env.config';
 import { corsOptions } from './util/cors.util';
+import { v1Router } from './routes/index.route';
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use('/api/v1', v1Router);
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({ message: 'Server is alive' });
+  return;
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
