a9c5bd89023a05799e4274cc202ba87e0b0e0f95
Aniket <aniketraj28042003@gmail.com>
2025-06-21 22:41:51 +0530

    add: repository: added getCityByAirportId functionality

--- src/repository/city.repository.ts
+++ src/repository/city.repository.ts
@@ -137,4 +137,39 @@ export class CityRepository {
       await client.release();
     }
   }
+
+  async getCityByAirportId(airportId: string): Promise<ICityWithCountry> {
+    const client: PoolClient = await this.pool.connect();
+    try {
+      const query = `SELECT c.*, co.name AS country_name, co.code AS country_code, co.created_at AS country_created_at, co.updated_at AS country_updated_at 
+                     FROM cities c 
+                     INNER JOIN countries co ON c.country_id = co.id 
+                     INNER JOIN airports a ON c.id = a.city_id 
+                     WHERE a.id = $1`;
+      const result = await client.query(query, [airportId]);
+      const city = result.rows[0];
+      if (city === null) {
+        throw new Error(`City with airport id ${airportId} not found`);
+      }
+      const cityResponse: ICityWithCountry = {
+        id: city.id,
+        name: city.name,
+        country: {
+          id: city.country_id,
+          code: city.country_code,
+          name: city.country_name,
+          created_at: city.country_created_at,
+          updated_at: city.country_updated_at,
+        },
+        created_at: city.created_at,
+        updated_at: city.updated_at,
+      };
+      return cityResponse;
+    } catch (error) {
+      console.log('Error in CityRepo: getCityByAirportId:', error);
+      throw error;
+    } finally {
+      await client.release();
+    }
+  }
 }
